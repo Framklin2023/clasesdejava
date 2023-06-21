3a6088bb51d0b3163680b71eb4c7afee2c3afa40
Framklin2023 <137226922+Framklin2023@users.noreply.github.com>
2023-06-21 18:42:44 -0500

    primera modificacion

--- uno.java
+++ uno.java
@@ -1,5 +1,8 @@
 public class uno{
     public static void main(String[] args) {
+        int uno;
+        uno=12;
         System.out.println("funciona");
+        System.out.println("entero  :"+uno);
     }
 }

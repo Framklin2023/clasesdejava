3bf0e817d01ad06e2ff065dbae448e553caae6f2
Framklin2023 <137226922+Framklin2023@users.noreply.github.com>
2023-06-21 19:38:59 -0500

    segunda modificacion

--- uno.java
+++ uno.java
@@ -2,7 +2,25 @@ public class uno{
     public static void main(String[] args) {
         int uno;
         uno=12;
-        System.out.println("funciona");
-        System.out.println("entero  :"+uno);
+        class Clase{
+            private String nombre;
+            public void setNombre(String nombre) {
+                this.nombre = nombre;
+            }
+            public String getNombre() {
+                return nombre;
+            }
+        }
+        Clase c1 = new Clase();
+        c1.setNombre("uno");
+        Clase c2=c1;
+        System.out.println(c2.getNombre());
+        c1.setNombre("dos");
+        System.out.println(c1.getNombre());
+        System.out.println(c2.getNombre());
+        
+
+       /*  System.out.println("funciona");
+        System.out.println("entero  :"+uno);*/
     }
 }
